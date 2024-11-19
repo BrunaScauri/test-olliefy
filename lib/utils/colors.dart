e0badabf3807cde268bae323e05a0a7aff14a0d5
brunascauri <brunascauri@hotmail.com>
2024-11-19 12:52:27 -0300

    feat: edit color sheet

--- lib/utils/colors.dart
+++ lib/utils/colors.dart
@@ -6,7 +6,11 @@ class AppColors {
   static const Color mobileSearchColor = Color.fromRGBO(38, 38, 38, 1);
   static const Color blueColor = Color.fromRGBO(0, 149, 246, 1);
   static const Color primaryColor = Color.fromRGBO(236, 212, 68, 1);
-  static const Color primaryGold = Color.fromRGBO(222, 199, 55, 1);
+  static const Color primaryGold60 = Color.fromRGBO(222, 199, 55, 1);
+  static const Color primaryGold40 = Color.fromRGBO(229, 210, 95, 1);
+  static const Color primaryGold20 = Color.fromRGBO(242, 159, 44, 1);
+  static const Color primaryGold20Stepper = Color.fromRGBO(242, 233, 175, 1);
+  static const Color primaryGold10 = Color.fromRGBO(248, 244, 215, 1);
   static const Color white = Color.fromRGBO(255, 255, 255, 1);
   static const Color black = Colors.black;
   static const Color secondaryColor = Colors.grey;
