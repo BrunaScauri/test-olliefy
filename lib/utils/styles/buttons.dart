c3ac107a97ea83a56bb3285e280e566dcf99e3f6
brunascauri <brunascauri@hotmail.com>
2024-11-28 13:28:19 -0300

    feat: button style sheet

---
+++ lib/utils/styles/buttons.dart
@@ -0,0 +1,44 @@
+import 'package:flutter/material.dart';
+import 'package:test_olliefy/utils/colors.dart';
+import 'package:google_fonts/google_fonts.dart';
+
+class ButtonStyles {
+  static ButtonStyle elevatedButtonEnabled (){
+    return ElevatedButton.styleFrom(
+      backgroundColor: AppColors.primaryGold60,
+      minimumSize: Size(350, 60),
+      shape: RoundedRectangleBorder(
+        borderRadius: BorderRadius.circular(8),
+      ),
+      elevation: 0.0,
+      shadowColor: Colors.transparent,
+    );
+  }
+  static ButtonStyle elevatedButtonDisabled (){
+    return ElevatedButton.styleFrom(
+      backgroundColor: AppColors.buttonDisabled12,
+      minimumSize: Size(350, 60),
+      shape: RoundedRectangleBorder(
+        borderRadius: BorderRadius.circular(8),
+      ),
+      elevation: 0.0,
+      shadowColor: Colors.transparent,
+    );
+  }
+  
+  static TextStyle elevatedButtonTextEnabled (){
+    return GoogleFonts.openSans(
+      fontSize: 16,
+      fontWeight: FontWeight.bold,
+      color: AppColors.black,
+    );
+  }
+
+  static TextStyle elevatedButtonTextDisabled (){
+    return GoogleFonts.openSans(
+      fontSize: 16,
+      fontWeight: FontWeight.bold,
+      color: AppColors.primaryGray50,
+    );
+  }
+}
