fc3df50028fd8e288a7556227713750ff8f7976b
brunascauri <brunascauri@hotmail.com>
2024-11-29 17:15:40 -0300

    WIP: permissions page

--- lib/modal/register_steps.dart
+++ lib/modal/register_steps.dart
@@ -192,25 +192,123 @@ class _PasswordState extends State<Password> {
     );
   }
 }
-class Permissions extends StatelessWidget {
+class Permissions extends StatefulWidget {
   @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      backgroundColor: AppColors.white,
-      body: Center(
-        child: Container(
-          child: OutlinedButton(
-            onPressed: () {
-              Navigator.of(context).pushReplacement(
-                PageRouteBuilder(
-                  pageBuilder:(context, animation, secondaryAnimation) => MainScreen(),
-                )
-              );
-            },
-            child: Text('main screen')
+  _PermissionsState createState() => _PermissionsState();
+}
+
+class _PermissionsState extends State<Permissions> {
+  // final TextEditingController _passwordController = TextEditingController();
+
+  // @override
+  // void dispose() {
+  //   _passwordController.dispose();
+  //   super.dispose();
+  // }
+
+  @override
+    Widget build(BuildContext context) {
+    return ChangeNotifierProvider<UserModal>(
+      create: (context) => UserModal(),
+          child: Scaffold(
+            backgroundColor: Colors.white,
+            body: Column(
+              children: [
+                Consumer<UserModal>(
+                builder: (context, modal, child) {
+                  return Padding(
+                    padding: const EdgeInsets.symmetric(horizontal: 16.0),
+                    child: Column(
+                      children: [
+                        Container(
+                          child: Column(
+                            children: [
+                              SizedBox(height: 20),
+                              Container(
+                                child: Image(image: AssetImage('assets/permissions_page_hero.png'), height: 160, width: 160),
+                              ),
+                              SizedBox(height: 40),
+                              Container(
+                                  child: RichText(
+                                    text:TextSpan(style: TextStyle(fontSize: 24, fontWeight: FontWeight.bold, color: Colors.black), children: [
+                                      TextSpan(text:'For a more fun experience, we need to activate some permissions.'),
+                                    ])
+                                  )
+                              ),
+                            ]
+                          )
+                        ),
+                        
+                        SizedBox(height: 40),
+                        Container(
+                          child: Column(
+                            children: [
+                              Row(
+                                children: [
+                                  Container(
+                                    child: Image(image: AssetImage('assets/map_pin.png'), height: 30, width: 30),
+                                  ),
+                                  SizedBox(width: 16),
+                                  RichText(
+                                  text:TextSpan(style: GoogleFonts.openSans(fontSize: 14, fontWeight: FontWeight.w400, color: Colors.black), children: [
+                                    TextSpan(text:'Location'),
+                                    ])
+                                  ),
+                                ]
+                              ),
+                              SizedBox(height: 16),
+                              Row(
+                                children: [
+                                  Container(
+                                    child: Image(image: AssetImage('assets/camera.png'), height: 30, width: 30),
+                                  ),
+                                  SizedBox(width: 16),
+                                  RichText(
+                                  text:TextSpan(style: GoogleFonts.openSans(fontSize: 14, fontWeight: FontWeight.w400, color: Colors.black), children: [
+                                    TextSpan(text:'Camera'),
+                                    ])
+                                  ),
+                                ]
+                              ),
+                              SizedBox(height: 16),
+                              Row(
+                                children: [
+                                  Container(
+                                    child: Image(image: AssetImage('assets/gallery.png'), height: 30, width: 30),
+                                  ),
+                                  SizedBox(width: 16),
+                                  RichText(
+                                  text:TextSpan(style: GoogleFonts.openSans(fontSize: 14, fontWeight: FontWeight.w400, color: Colors.black), children: [
+                                    TextSpan(text:'Photo gallery'),
+                                    ])
+                                  ),
+                                ]
+                              ),
+                              SizedBox(height: 16),
+                              Row(
+                                children: [
+                                  Container(
+                                    child: Image(image: AssetImage('assets/phone.png'), height: 30, width: 30),
+                                  ),
+                                  SizedBox(width: 16),
+                                  RichText(
+                                  text:TextSpan(style: GoogleFonts.openSans(fontSize: 14, fontWeight: FontWeight.w400, color: Colors.black), children: [
+                                    TextSpan(text:'Contacts'),
+                                    ])
+                                  ),
+                                ]
+                              ),
+                            ]
+                          )
+                        )
+                      ]
+                    )
+                  );
+                },
+              )
+              ]
+            )
           )
-        )
-      ),
-    );
+        );
   }
 }
