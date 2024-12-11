be4aa65985975c7e6c02fbd601866ba8aae97a7f
brunascauri <brunascauri@hotmail.com>
2024-12-11 16:44:06 -0300

    style: edit primaryWhite and primaryGold30, removes 'secondaryColor'

--- lib/utils/colors.dart
+++ lib/utils/colors.dart
@@ -1,20 +1,19 @@
 import 'package:flutter/material.dart';
 
 class AppColors {
-  static const Color primaryColor = Color.fromRGBO(236, 212, 68, 1);
-  static const Color primaryWhite = Color.fromRGBO(255, 255, 255, 1);
 
   static const Color primaryGold70 = Color.fromRGBO(134, 118, 32, 1);
   static const Color primaryGold60 = Color.fromRGBO(222, 199, 55, 1);
   static const Color primaryGold40 = Color.fromRGBO(229, 210, 95, 1);
+  static const Color primaryGold30 = Color.fromRGBO(236, 212, 68, 1);
   static const Color primaryGold20 = Color.fromRGBO(242, 159, 44, 1);
   static const Color primaryGold20Stepper = Color.fromRGBO(242, 233, 175, 1);
   static const Color primaryGold10 = Color.fromRGBO(248, 244, 215, 1);
 
   static const Color primaryBlack = Color.fromRGBO(0, 0, 0, 1);
-  static const Color secondaryColor = Colors.grey;
   static const Color primaryGray50 = Color.fromRGBO(74, 71, 57, 1);
   static const Color primaryGray30 = Color.fromRGBO(132, 129, 117, 1);
   static const Color primaryGray10 = Color.fromRGBO(74, 71, 57, 1);
   static const Color buttonDisabled12 = Color.fromRGBO(74, 71, 57, 0.12);
+  static const Color primaryWhite = Color.fromRGBO(255, 255, 255, 1);
 }
