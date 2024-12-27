fd2e56c08da07d81b62c7beb3e5c461ed8071873
brunascauri <brunascauri@hotmail.com>
2024-12-27 18:37:33 -0300

    feat: add user store tab and marketplace button

--- lib/screens/profile/user_store.dart
+++ lib/screens/profile/user_store.dart
@@ -19,23 +19,29 @@ class _UserStoreState extends State<UserStore> {
         child: Column (
           children: [
             Padding(
-            padding: const EdgeInsets.all(15.0),
+              padding: const EdgeInsets.symmetric(vertical: 24, horizontal: 16),
               child: Container(
                 constraints: BoxConstraints(maxWidth: 370),
-                child: ElevatedButton(
+                child: OutlinedButton(
                   onPressed: () {
                   },
-                  style: ButtonStyles.socialsButton(
+                  style: ButtonStyles.marketplaceButton(
                     backgroundColor: AppColors.primaryWhite,
                   ),
                   child: Row(
                     mainAxisAlignment: MainAxisAlignment.center,
                     children: [
+                      Icon(
+                        Icons.add,
+                        color: AppColors.primaryGray50,
+                        size: 20,
+                      ),
+                      SizedBox(width: 2),
                       RichText(
                         text: TextSpan(
                           style: TextStyles.elevatedButtonText(color: AppColors.primaryGray50),
                           children: [
-                            TextSpan(text: 'User Store'),
+                            TextSpan(text: 'Sell in the marketplace'),
                           ],
                         ),
                       ),
