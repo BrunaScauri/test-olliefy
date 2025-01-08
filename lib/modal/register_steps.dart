e089ad492bf8679f92615d16b6f44aa436acf1c3
brunascauri <brunascauri@hotmail.com>
2025-01-08 16:02:03 -0300

    feat: add suggestedProfiles page

--- lib/modal/register_steps.dart
+++ lib/modal/register_steps.dart
@@ -8,6 +8,9 @@ import 'package:test_olliefy/utils/colors.dart';
 import 'package:test_olliefy/utils/styles/fields.dart';
 import 'package:test_olliefy/utils/styles/buttons.dart';
 import 'package:test_olliefy/components/molecules/permissions_card.dart';
+import 'package:test_olliefy/components/organisms/suggested_profiles_card.dart';
+import 'package:test_olliefy/components/atoms/follow_button.dart';
+import 'package:test_olliefy/components/atoms/following_button.dart';
 
 class ProfileName extends StatefulWidget {
   @override
@@ -553,4 +556,90 @@ class _PermissionsState extends State<Permissions> {
       )
     );
   }
+}
+
+class SuggestedProfiles extends StatefulWidget {
+  @override
+  _SuggestedProfilesState createState() => _SuggestedProfilesState();
+}
+
+class _SuggestedProfilesState extends State<SuggestedProfiles> {
+  final List<Map<String, dynamic>> items = [
+    {'imagePath': 'assets/profile_page/icons/example_pfp/0.jpeg', 'profileName': 'a way too long name', 'following': false},
+    {'imagePath': 'assets/profile_page/icons/example_pfp/1.jpeg', 'profileName': 'this is a scrollable list', 'following': true},
+    {'imagePath': 'assets/profile_page/icons/example_pfp/2.jpeg', 'profileName': 'trickwizard', 'following': true},
+    {'imagePath': 'assets/profile_page/icons/example_pfp/3.jpeg', 'profileName': 'board_beast', 'following': true},
+    {'imagePath': 'assets/profile_page/icons/example_pfp/0.jpeg', 'profileName': 'sk8_legend', 'following': false},
+    {'imagePath': 'assets/profile_page/icons/example_pfp/1.jpeg', 'profileName': 'pavementripper', 'following': false},
+    {'imagePath': 'assets/profile_page/icons/example_pfp/2.jpeg', 'profileName': 'trickwizard', 'following': true},
+    {'imagePath': 'assets/profile_page/icons/example_pfp/3.jpeg', 'profileName': 'board_beast', 'following': false},
+    {'imagePath': 'assets/profile_page/icons/example_pfp/0.jpeg', 'profileName': 'sk8_legend', 'following': true},
+    {'imagePath': 'assets/profile_page/icons/example_pfp/1.jpeg', 'profileName': 'pavementripper', 'following': false},
+    {'imagePath': 'assets/profile_page/icons/example_pfp/2.jpeg', 'profileName': 'trickwizard', 'following': false},
+    {'imagePath': 'assets/profile_page/icons/example_pfp/3.jpeg', 'profileName': 'board_beast', 'following': true},
+    {'imagePath': 'assets/profile_page/icons/example_pfp/0.jpeg', 'profileName': 'sk8_legend', 'following': false},
+    {'imagePath': 'assets/profile_page/icons/example_pfp/1.jpeg', 'profileName': 'pavementripper', 'following': true},
+    {'imagePath': 'assets/profile_page/icons/example_pfp/2.jpeg', 'profileName': 'trickwizard', 'following': true},
+    {'imagePath': 'assets/profile_page/icons/example_pfp/3.jpeg', 'profileName': 'board_beast', 'following': true},
+    {'imagePath': 'assets/profile_page/icons/example_pfp/0.jpeg', 'profileName': 'sk8_legend', 'following': false},
+    {'imagePath': 'assets/profile_page/icons/example_pfp/1.jpeg', 'profileName': 'pavementripper', 'following': false},
+    {'imagePath': 'assets/profile_page/icons/example_pfp/2.jpeg', 'profileName': 'trickwizard', 'following': false},
+    {'imagePath': 'assets/profile_page/icons/example_pfp/3.jpeg', 'profileName': 'board_beast', 'following': false},
+  ];
+
+  @override
+Widget build(BuildContext context) {
+  return Scaffold(
+    backgroundColor: AppColors.primaryWhite,
+    body: Center(
+      child: Container(
+        constraints: BoxConstraints(maxWidth: 370),
+        child: Column(
+          children: [
+            const SizedBox(height: 10),
+            Padding(
+              padding: EdgeInsets.all(16.0),
+              child: RichText(
+                text: TextSpan(
+                  style: TextStyles.primaryHeader(),
+                  children: [
+                    TextSpan(text: 'Follow some skaters in your area'),
+                  ],
+                ),
+              ),
+            ),
+            const SizedBox(height: 20),
+            Expanded(
+              child: Consumer<UserModal>(
+                builder: (context, modal, child) {
+                  return ListView.separated(
+                    itemCount: items.length,
+                    itemBuilder: (context, index) {
+                      final item = items[index];
+                      return Row(
+                        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                        children: [
+                          SuggestedProfileCard(
+                            imagePath: item['imagePath'],
+                            profileName: item['profileName'],
+                          ),
+                          Padding(
+                            padding: EdgeInsets.only(right: 1.0),
+                            child: item['following'] == true ? FollowButton() : FollowingButton()
+                          )
+
+                        ]
+                      );
+                    },
+                    separatorBuilder: (BuildContext context, int index) => const SizedBox(height: 29),
+                  );
+                },
+              ),
+            ),
+          ],
+        ),
+      ),
+    ),
+  );
+}
 }
