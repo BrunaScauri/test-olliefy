15d1f7e6a28603cb501ef41e399c30764cddb877
brunascauri <brunascauri@hotmail.com>
2025-01-13 17:29:49 -0300

    style: update spacing, paddings and buttons styling

--- lib/modal/login_modal.dart
+++ lib/modal/login_modal.dart
@@ -6,6 +6,7 @@ import 'package:flutter_svg/svg.dart';
 import 'package:test_olliefy/utils/styles/fields.dart';
 import 'package:test_olliefy/utils/styles/buttons.dart';
 import 'package:test_olliefy/utils/styles/texts.dart';
+import 'package:test_olliefy/utils/styles/socials_button.dart';
 
 import 'package:test_olliefy/screens/map.dart';
 import 'package:test_olliefy/screens/main_screen.dart';
@@ -53,132 +54,98 @@ class _LoginModalState extends State<LoginModal> {
             decoration: BoxDecoration(
               color: Colors.grey[400],
               borderRadius: BorderRadius.circular(10),
-            ),
-              width: 40
+            ), width: 40,
           ),
-          SizedBox(height: 30),
-          Align(
-            alignment: Alignment.centerLeft,
-            child: Row(
-              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+          SizedBox(height: 24),
+          //'log in' title 
+          Row(
+            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+            children: [
+              Text(
+                'LOG IN',
+                style: TextsStyles.spacedGray(),
+              ),
+              IconButton(
+                icon: Icon(Icons.close),
+                iconSize: 24,
+                color: AppColors.primaryGray30,
+                onPressed: () {
+                  Navigator.pop(context);
+                },
+              ),
+            ]
+          ),
+          //textfield for login and button
+          Padding(
+            padding: EdgeInsets.symmetric(vertical: 35.0),
+            child: Column(
               children: [
-                Text(
-                  'LOG IN',
-                  style: TextsStyles.spacedGray(),
+                TextFormField(
+                    controller: _controller,
+                    onChanged: (value) {
+                    final emailRegExp = RegExp(
+                      r'^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$',
+                    );
+                    setState(() {
+                        _isButtonEnabled = emailRegExp.hasMatch(value);
+                      });
+                    },
+                    decoration: FormDecorations.textFieldDecoration(labelText: 'Phone, email or username'),
+                  ),
+                SizedBox(height: 16),
+                ElevatedButton(
+                  onPressed: _isButtonEnabled ? () {
+                    Navigator.of(context).pushReplacement(
+                      MaterialPageRoute(builder: (context) => Map()),
+                    );
+                  } : null,
+                  style: _isButtonEnabled ? ButtonStyles.elevatedButton(backgroundColor: AppColors.primaryBlack) : ButtonStyles.elevatedButton(backgroundColor: AppColors.buttonDisabled12),
+                  child: Row(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: [
+                      RichText(
+                        text: TextSpan(
+                          style: _isButtonEnabled 
+                          ? TextStyles.elevatedButtonText(color: AppColors.primaryWhite) 
+                          : TextStyles.elevatedButtonText(color: AppColors.primaryGray50),
+                          children: [
+                            TextSpan(text: 'Log in'),
+                          ],
+                        ),
+                      ),
+                    ]
+                  ),
                 ),
-                IconButton(
-                  icon: Icon(Icons.close),
-                  iconSize: 30,
-                  color: AppColors.primaryGray30,
+              ]
+            )
+          ),
+          Divider(height: 10, thickness: 0.8),
+          //socials buttons
+          Padding(
+            padding: EdgeInsets.symmetric(vertical: 40.0),
+            child: Column(
+              children: [
+                CustomButtons.socialsButton(
+                  backgroundColor: AppColors.primaryWhite,
+                  assetPath: 'assets/google_logo.svg',
+                  buttonText: 'Continue with Google',
+                  textColor: AppColors.primaryBlack,
                   onPressed: () {
-                    Navigator.pop(context);
                   },
                 ),
-              ]
-            )
-            
-          ),
-          SizedBox(height: 30),
-          Container(
-            child: TextFormField(
-              controller: _controller,
-              onChanged: (value) {
-              final emailRegExp = RegExp(
-                r'^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$',
-              );
-              setState(() {
-                  _isButtonEnabled = emailRegExp.hasMatch(value);
-                });
-              },
-              decoration: FormDecorations.textFieldDecoration(labelText: 'Phone, email or username'),
-            ),
-          ), 
-          SizedBox(height: 15),
-          ElevatedButton(
-            onPressed: _isButtonEnabled ? () {
-              Navigator.of(context).pushReplacement(
-                MaterialPageRoute(builder: (context) => Map()),
-              );
-            } : null,
-            style: _isButtonEnabled ? ButtonStyles.elevatedButton(backgroundColor: AppColors.primaryBlack) : ButtonStyles.elevatedButton(backgroundColor: AppColors.buttonDisabled12),
-            child: Row(
-              mainAxisAlignment: MainAxisAlignment.center,
-              children: [
-                RichText(
-                  text: TextSpan(
-                    style: _isButtonEnabled 
-                    ? TextStyles.elevatedButtonText(color: AppColors.primaryWhite) 
-                    : TextStyles.elevatedButtonText(color: AppColors.primaryGray50),
-                    children: [
-                      TextSpan(text: 'Log in'),
-                    ],
-                  ),
-                ),
-
-              ]
-            ),
-          ),
-            
-          const SizedBox(height: 10),
-          Divider(height: 60, thickness: 0.8),
-          ElevatedButton(
-            onPressed: () {
-            },
-            style: ButtonStyles.socialsButton(
-              backgroundColor: AppColors.primaryWhite,
-            ),
-            child: Row(
-              mainAxisAlignment: MainAxisAlignment.center,
-              children: [
-                Padding(
-                  padding: const EdgeInsets.only(right: 8),
-                  child: Image.asset(
-                    'assets/google_logo.png',
-                    height: 12,
-                    width: 12,
-                  ),
-                ),
-                RichText(
-                  text: TextSpan(
-                    style: TextStyles.elevatedButtonText(color: AppColors.primaryGray50),
-                    children: [
-                      TextSpan(text: 'Continue with Google'),
-                    ],
-                  ),
-                ),
-              ],
-            ),
-          ),
-          const SizedBox(height: 15),
-          ElevatedButton(
-            onPressed: () {},
-            style: ButtonStyles.socialsButton(
-              backgroundColor: AppColors.primaryBlack
-            ),
-            child: Row(
-              mainAxisAlignment: MainAxisAlignment.center,
-              children: [
-                Padding(
-                  padding: const EdgeInsets.only(right: 8),
-                  child: SvgPicture.asset(
-                    'assets/Apple_logo.svg',
-                    colorFilter: const ColorFilter.mode(AppColors.primaryWhite, BlendMode.srcIn),
-                    height: 15,
-                  ),
-                ),
-                RichText(
-                  text: TextSpan(
-                    style: TextStyles.elevatedButtonText(color: AppColors.primaryWhite),
-                    children: [
-                      TextSpan(text: 'Continue with Apple'),
-                    ],
-                  ),
+                const SizedBox(height: 16),
+                CustomButtons.socialsButton(
+                  backgroundColor: AppColors.primaryBlack,
+                  assetPath: 'assets/apple_logo.svg',
+                  buttonText: 'Continue with Apple',
+                  textColor: AppColors.primaryWhite,
+                  onPressed: () {
+                  },
                 ),
 
               ]
             )
           ),
-          const SizedBox(height: 15),
           RichText(
             text: TextSpan(
               style: GoogleFonts.openSans(fontSize: 14, color: Colors.black),
@@ -197,6 +164,7 @@ class _LoginModalState extends State<LoginModal> {
               ],
             ),
           ),
+          Flexible(child: Container())
         ],
       ),
     );
