c76d39319eba140a3dcf0f036c42b1cd9ed53554
brunascauri <brunascauri@hotmail.com>
2025-01-13 10:10:34 -0300

    fix: updated assets

--- lib/screens/splashscreen.dart
+++ lib/screens/splashscreen.dart
@@ -50,10 +50,9 @@ class Splashscreen extends StatefulWidget {
         child: 
         Column(children: [  
         Flexible(flex: 5, child: Container()),
-        // SvgPicture.asset('assets/skateboard_only_logo.svg', colorFilter: const ColorFilter.mode(AppColors.primaryGold30, BlendMode.srcIn), height: 100),
+        SvgPicture.asset('assets/skateboard_only_logo.svg', colorFilter: const ColorFilter.mode(AppColors.primaryGold30, BlendMode.srcIn), height: 100),
         const SizedBox(height: 48),
         Flexible(flex: 2, child: Container()),
-        // const Image(image: AssetImage('assets/Animation.gif'), height: 100, width: 150),
         const Image(image: AssetImage('assets/splashscreen/Animation.gif'), height: 100, width: 200),
         Container(
           width: 80,
