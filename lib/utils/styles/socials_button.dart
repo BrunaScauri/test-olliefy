6a7356f9bc0d8ece86f568f461853fe949ca5c3e
brunascauri <brunascauri@hotmail.com>
2025-01-15 15:42:35 -0300

    feat: add socialButtons class

---
+++ lib/utils/styles/socials_button.dart
@@ -0,0 +1,47 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_svg/flutter_svg.dart';
+import 'package:test_olliefy/utils/colors.dart';
+import 'package:test_olliefy/utils/styles/buttons.dart';
+
+class CustomButtons {
+  static ElevatedButton socialsButton({
+    required Color backgroundColor,
+    required String assetPath,
+    required String buttonText,
+    required VoidCallback onPressed,
+    required Color textColor,
+  }) {
+    return ElevatedButton(
+      onPressed: onPressed,
+      style: ElevatedButton.styleFrom(
+        backgroundColor: backgroundColor,
+        minimumSize: const Size(350, 60),
+        shadowColor: Colors.black,
+        elevation: 3.0,
+        shape: RoundedRectangleBorder(
+          borderRadius: BorderRadius.circular(8),
+        ),
+      ),
+      child: Row(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: [
+          Padding(
+            padding: const EdgeInsets.only(right: 8),
+            child: SvgPicture.asset(
+              assetPath,
+              height: 18,
+            ),
+          ),
+          RichText(
+            text: TextSpan(
+              style: TextStyles.elevatedButtonText(color: textColor),
+              children: [
+                TextSpan(text: buttonText),
+              ],
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+}
