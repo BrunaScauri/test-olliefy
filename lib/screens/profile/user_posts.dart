87133adf47888b2ee17a97f13691fac93af67fb7
brunascauri <brunascauri@hotmail.com>
2025-01-16 19:55:12 -0300

    feat: add user posts section

--- lib/screens/profile/user_posts.dart
+++ lib/screens/profile/user_posts.dart
@@ -1,10 +1,9 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_svg/svg.dart';
 
 import 'package:test_olliefy/utils/colors.dart';
 import 'package:test_olliefy/utils/styles/texts.dart';
 import 'package:test_olliefy/utils/styles/buttons.dart';
-
-import 'package:test_olliefy/screens/main_screen.dart';
 
 class UserPosts extends StatefulWidget {
   @override
@@ -12,45 +11,86 @@ class UserPosts extends StatefulWidget {
 }
 
 class _UserPostsState extends State<UserPosts> {
+
+  final List<Map<String, dynamic>> items = [
+      {'thumbnail': '1.jpeg', 'type': 'image'},
+      {'thumbnail': '2.jpeg', 'type': 'event'},
+      {'thumbnail': '3.jpeg', 'type': 'video'},
+      {'thumbnail': '4.jpeg', 'type': 'video'},
+      {'thumbnail': '5.jpeg', 'type': 'video'},
+      {'thumbnail': '6.jpeg', 'type': 'event'},
+    ];
+
+    final imageIcon = 'assets/profile_page/icons/image_icon.svg';
+    final videoIcon = 'assets/profile_page/icons/video_icon.svg';
+    final eventIcon = 'assets/profile_page/icons/event_icon.svg';
+
   @override
   Widget build(BuildContext context) {
-    return Center(
-      child: Container(
-        child: Column (
-          children: [
-            Padding(
-            padding: const EdgeInsets.all(15.0),
-              child: Container(
-                constraints: BoxConstraints(maxWidth: 370),
-                child: ElevatedButton(
-                  onPressed: () {
-                    Navigator.push(
-                      context,
-                      MaterialPageRoute(builder: (context) => MainScreen()),
-                    );
-                  },
-                  style: ButtonStyles.socialsButton(
-                    backgroundColor: AppColors.primaryWhite,
-                  ),
-                  child: Row(
-                    mainAxisAlignment: MainAxisAlignment.center,
+    return Padding(
+      padding: EdgeInsets.symmetric(vertical: 24, horizontal: 16),
+      child: Expanded(
+        child: GridView.builder(
+          gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
+            crossAxisCount: 3,
+            crossAxisSpacing: 5.0,
+            mainAxisSpacing: 5.0,
+            childAspectRatio: 0.65,
+          ),
+          padding: const EdgeInsets.symmetric(horizontal: 8),
+          itemCount: items.length,
+          itemBuilder: (context, index) {
+            final item = items[index];
+            return GridTile(
+              child: Column(
+                children: [
+                  Stack(
                     children: [
-                      RichText(
-                        text: TextSpan(
-                          style: TextStyles.elevatedButtonText(color: AppColors.primaryGray50),
-                          children: [
-                            TextSpan(text: 'User Posts'),
-                          ],
+                      Container(
+                        height: 158,
+                        decoration: BoxDecoration(
+                          borderRadius: BorderRadius.circular(6),
+                          border: Border.all(
+                            color: AppColors.primaryGray5,
+                            width: 1.0,
+                          ),
+                          image: DecorationImage(
+                            image: AssetImage('assets/profile_page/example_posts/${item['thumbnail']}'),
+                            fit: BoxFit.cover,
+                          ),
                         ),
                       ),
-                    ],
+                      Positioned(
+                        top: 8.7,
+                        right: 8.4,
+                        child:  item['type'] == 'image'
+                        ? SvgPicture.asset(
+                            imageIcon,
+                            width: 13.0,
+                            height: 13.0,
+                          )
+                        : item['type'] == 'video'
+                          ? SvgPicture.asset(
+                              videoIcon,
+                              width: 13.0,
+                              height: 13.0,
+                            )
+                        : item['type'] == 'event'
+                            ? SvgPicture.asset(
+                                eventIcon,
+                                width: 13.0,
+                                height: 13.0,
+                              )
+                        : Container(),
+                      ),
+                    ]
                   ),
-                ),
-              )
-            ),
-          ]
-        )
-      )
+                ],
+              ),
+            );
+          },
+        ),
+      ),
     );
   }
 }
