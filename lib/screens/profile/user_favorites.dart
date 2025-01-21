df25ed963114d55a01fc28170cdacd4af2ce9071
brunascauri <brunascauri@hotmail.com>
2025-01-21 18:25:57 -0300

    fix: tabs incator on favorites page

--- lib/screens/profile/user_favorites.dart
+++ lib/screens/profile/user_favorites.dart
@@ -15,6 +15,8 @@ class UserFavorites extends StatefulWidget {
 }
 
 class _UserFavoritesState extends State<UserFavorites> {
+  int currentIndex = 0;
+
 	@override
 	Widget build(BuildContext context) {
     return Scaffold(
@@ -41,64 +43,68 @@ class _UserFavoritesState extends State<UserFavorites> {
               DefaultTabController(
                 initialIndex: 0,
                 length: 3,
-                child: Center(
-                  child: Container(
-                    child: Column(
-                      children: [
-                        TextFormField(
-                          decoration: InputDecoration(
-                            prefixIcon: Icon(Icons.search, color: Colors.grey),
-                            hintText: 'Search spots or items',
-                            border: OutlineInputBorder(
-                              borderRadius: BorderRadius.circular(3.0),
-                              borderSide: BorderSide(color: AppColors.primaryGold70),
-                            ),
-                            focusedBorder: OutlineInputBorder(
-                              borderSide: BorderSide(color: AppColors.primaryGold70, width: 3.0),
-                            ),
-                            contentPadding: EdgeInsets.symmetric(vertical: 12.0, horizontal: 16.0),
-                          ),
-                          onChanged: (value) {
-                            print("Search query: $value");
-                          },
+                child: Column(
+                  children: [
+                    TextFormField(
+                      decoration: InputDecoration(
+                        prefixIcon: Icon(Icons.search, color: Colors.grey),
+                        hintText: 'Search spots or items',
+                        border: OutlineInputBorder(
+                          borderRadius: BorderRadius.circular(3.0),
+                          borderSide: BorderSide(color: AppColors.primaryGold70),
                         ),
-                        SizedBox(height: 28),
-                        Container(
-                          width: 400,
-                          child: TabBar(
-                            tabs: [
-                              Tab(child: Align(alignment: Alignment.centerLeft, child: Text(
-                                'Spots'
-                              ))),
-                              Tab(text: 'Marketplace'),
-                              Tab(child: Align(alignment: Alignment.centerRight, child: Text(
-                                'Posts'
-                              ))),
-                            ],
-                            indicatorColor: AppColors.primaryGold70,
-                            // indicator: CustomProfileTabIndicator(),
-                            labelColor: AppColors.primaryBlack,
-                            labelStyle: TextStyle(fontWeight: FontWeight.bold),
-                            unselectedLabelStyle: TextStyle(fontWeight: FontWeight.normal),
-                          )
+                        focusedBorder: OutlineInputBorder(
+                          borderSide: BorderSide(color: AppColors.primaryGold70, width: 3.0),
                         ),
-                        SizedBox(
-                          height: 700,
-                          child: TabBarView(
-                            children: <Widget>[
-                              FavoritesSpots(),
-                              FavoritesMarketplace(),
-                              FavoritesPosts(),
-                            ]
-                          )
-                        ),
-                      ]
-                    )
-                  )
-                )
-              )
+                        contentPadding: EdgeInsets.symmetric(vertical: 12.0, horizontal: 16.0),
+                      ),
+                      onChanged: (value) {
+                        print("Search query: $value");
+                      },
+                    ),
+                    SizedBox(height: 28),
+                    Container(
+                      child: TabBar(
+                        onTap: (index) {
+                          setState(() {
+                            currentIndex = index;
+                          });
+                        },
+                        tabs: [
+                          Tab(child: Align(alignment: Alignment.centerLeft, child: Text(
+                            'Spots'
+                          ))),
+                          Tab(text: 'Marketplace'),
+                          Tab(child: Align(alignment: Alignment.centerRight, child: Text(
+                            'Posts'
+                          ))),
+                        ],
+                        indicatorColor: AppColors.primaryGold70,
+                        labelPadding: EdgeInsets.zero,
+                        indicatorPadding: currentIndex == 0 ? EdgeInsets.only(right: 80.0)
+                        : currentIndex == 1 ? EdgeInsets.zero
+                        : currentIndex == 2 ? EdgeInsets.only(left: 79.0)
+                        : EdgeInsets.zero,
+                        labelColor: AppColors.primaryBlack,
+                        labelStyle: TextStyle(fontWeight: FontWeight.bold),
+                        unselectedLabelStyle: TextStyle(fontWeight: FontWeight.normal),
+                      ),
+                    ),
+                    SizedBox(
+                      height: 700,
+                      child: TabBarView(
+                        children: <Widget>[
+                          FavoritesSpots(),
+                          FavoritesMarketplace(),
+                          FavoritesPosts(),
+                        ]
+                      )
+                    ),
+                  ]
+                )  
+              ),
             ]
-          ),
+          )
         )
       ),
     );
