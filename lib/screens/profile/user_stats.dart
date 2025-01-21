a60d85657a1a399c8ef0fcad6a4e70cebd256098
brunascauri <brunascauri@hotmail.com>
2025-01-21 19:40:40 -0300

    feat: add event cards and fix page scroll, WIP: scroll entire page as one

--- lib/screens/profile/user_stats.dart
+++ lib/screens/profile/user_stats.dart
@@ -6,6 +6,7 @@ import 'package:test_olliefy/utils/styles/buttons.dart';
 
 import 'package:test_olliefy/screens/profile/add_article_on_stats.dart';
 import 'package:test_olliefy/components/atoms/user_stats_card_description.dart';
+import 'package:test_olliefy/components/atoms/event_cards.dart';
 
 class UserStats extends StatefulWidget {
   @override
@@ -18,71 +19,79 @@ class _UserStatsState extends State<UserStats> {
     {'image': '2.png', 'itemName': 'Stage 11 Bar Flat Black Standard Independent', 'itemType': 'Truck',},
     {'image': '3.jpeg', 'itemName': 'Friend Of Sk8 Like A Girl Classic Natural 55mm Spitfire', 'itemType': 'Wheels',},
     {'image': '4.png', 'itemName': 'Red Bones Big Balls', 'itemType': 'Bearings',},
+    {'image': '1.jpeg', 'itemName': 'Piled Willms 8.3″X32″ Sk8Mafia Deck', 'itemType': 'Deck',},
+    {'image': '2.png', 'itemName': 'Stage 11 Bar Flat Black Standard Independent', 'itemType': 'Truck',},
+    {'image': '3.jpeg', 'itemName': 'Friend Of Sk8 Like A Girl Classic Natural 55mm Spitfire', 'itemType': 'Wheels',},
+    {'image': '4.png', 'itemName': 'Red Bones Big Balls', 'itemType': 'Bearings',},
     
   ];
 
   @override
   Widget build(BuildContext context) {
-    return Padding(
+  return SingleChildScrollView(
+    child: Padding(
       padding: EdgeInsets.only(top: 48, bottom: 20),
       child: Column(
-      crossAxisAlignment: CrossAxisAlignment.start,
-      children: [
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: [
           Text('SETUP', style: TextsStyles.spacedGray()),
-          //space for the products
-          Expanded(
-            child: GridView.builder(
-            // physics: const NeverScrollableScrollPhysics(),
-              gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
-                crossAxisCount: 1,
-                crossAxisSpacing: 8.0,
-                mainAxisSpacing: 10.0,
-                childAspectRatio: 4,
-              ),
-              itemCount: items.length,
-              itemBuilder: (context, index) {
-                final item = items[index];
-                return GridTile(
-                    child: Row(
-                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                      children: [
-                        //prod image
-                        Container(
-                          height: 80,
-                          width: 80,
-                          decoration: BoxDecoration(
-                            borderRadius: BorderRadius.circular(4),
-                            border: Border.all(
-                              color: AppColors.primaryGray5,
-                              width: 1.0,
-                            ),
-                            image: DecorationImage(
-                              image: AssetImage('assets/profile_page/example_stats_items/${item['image']}'),
-                              fit: BoxFit.contain,
-                            ),
-                          ),
-                        ),
-                        //prod desc
-                        Container(
-                          width: 190,
-                          height: 90,
-                          child: UserStatsCardDescription(
-                            itemName: item['itemName'],
-                            itemType: item['itemType'],
-                          ),
-                        ),
-                        IconButton(
-                          icon: Image.asset('assets/profile_page/icons/pencil_icon.png', width: 18),
-                          // iconSize: 20,
-                          onPressed: () {
-                          },
-                        ),
-                      ],
-                  )
-                );
-              },
+          SizedBox(height: 16),
+          GridView.builder(
+            shrinkWrap: true,
+            physics: NeverScrollableScrollPhysics(),
+            gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
+              crossAxisCount: 1,
+              crossAxisSpacing: 8.0,
+              mainAxisSpacing: 10.0,
+              childAspectRatio: 4,
             ),
+            itemCount: items.length,
+            itemBuilder: (context, index) {
+              final item = items[index];
+              return GridTile(
+                child: Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  children: [
+                    // Product image
+                    Container(
+                      height: 80,
+                      width: 80,
+                      decoration: BoxDecoration(
+                        borderRadius: BorderRadius.circular(4),
+                        border: Border.all(
+                          color: AppColors.primaryGray5,
+                          width: 1.0,
+                        ),
+                        image: DecorationImage(
+                          image: AssetImage(
+                              'assets/profile_page/example_stats_items/${item['image']}'),
+                          fit: BoxFit.contain,
+                        ),
+                      ),
+                    ),
+                    // Product description
+                    Container(
+                      width: 190,
+                      height: 90,
+                      child: UserStatsCardDescription(
+                        itemName: item['itemName'],
+                        itemType: item['itemType'],
+                      ),
+                    ),
+                    IconButton(
+                      icon: Image.asset(
+                        'assets/profile_page/icons/pencil_icon.png',
+                        width: 18,
+                      ),
+                      onPressed: () {
+                      },
+                    ),
+                  ],
+                ),
+              );
+            },
           ),
+          SizedBox(height: 16),
           OutlinedButton(
             onPressed: () {
               showModalBottomSheet(
@@ -107,7 +116,8 @@ class _UserStatsState extends State<UserStats> {
                 SizedBox(width: 4),
                 RichText(
                   text: TextSpan(
-                    style: TextStyles.elevatedButtonText(color: AppColors.primaryGray10),
+                    style: TextStyles.elevatedButtonText(
+                        color: AppColors.primaryGray10),
                     children: [
                       TextSpan(text: 'Add article to setup'),
                     ],
@@ -116,8 +126,13 @@ class _UserStatsState extends State<UserStats> {
               ],
             ),
           ),
+          SizedBox(height: 48),
+          Text('EVENTS', style: TextsStyles.spacedGray()),
+          EventCards(),
         ],
       ),
-    );
-  }
+    ),
+  );
+}
+
 }
