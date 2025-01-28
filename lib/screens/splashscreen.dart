905ae8c4c7423d9ac268501bc80bc8a8448e893f
brunascauri <brunascauri@hotmail.com>
2025-01-28 14:31:09 -0300

    fix: skateboard_only_logo height on page and indentation

--- lib/screens/splashscreen.dart
+++ lib/screens/splashscreen.dart
@@ -42,26 +42,26 @@ class Splashscreen extends StatefulWidget {
     super.dispose();
   }
 
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      backgroundColor: const Color.fromRGBO(16, 16, 15, 1),
-      body: SafeArea( 
-        child: 
-        Column(children: [  
-        Flexible(flex: 5, child: Container()),
-        SvgPicture.asset('assets/skateboard_only_logo.svg', colorFilter: const ColorFilter.mode(AppColors.primaryGold30, BlendMode.srcIn), height: 100),
-        const SizedBox(height: 48),
-        Flexible(flex: 2, child: Container()),
-        const Image(image: AssetImage('assets/splashscreen/Animation.gif'), height: 100, width: 200),
-        Container(
-          width: 80,
-          child: LinearProgressIndicator(value: _progressValue, valueColor: AlwaysStoppedAnimation<Color>(Color.fromRGBO(222, 199, 55, 1)), backgroundColor: Colors.white),
-        ),
-        const SizedBox(height: 40),
-        ],)
-      )
-    );
-  }
+    @override
+    Widget build(BuildContext context) {
+      return Scaffold(
+        backgroundColor: const Color.fromRGBO(16, 16, 15, 1),
+        body: SafeArea( 
+          child: 
+          Column(children: [  
+          Flexible(flex: 5, child: Container()),
+          SvgPicture.asset('assets/skateboard_only_logo.svg', colorFilter: const ColorFilter.mode(AppColors.primaryGold30, BlendMode.srcIn), height: 100),
+          const SizedBox(height: 48),
+          Flexible(flex: 4, child: Container()),
+          const Image(image: AssetImage('assets/splashscreen/Animation.gif'), height: 100, width: 200),
+          Container(
+            width: 80,
+            child: LinearProgressIndicator(value: _progressValue, valueColor: AlwaysStoppedAnimation<Color>(Color.fromRGBO(222, 199, 55, 1)), backgroundColor: Colors.white),
+          ),
+          const SizedBox(height: 40),
+          ],)
+        )
+      );
+    }
 
   }
