a0a5f8fbda936cf3742ee3799fbe1f7597f3de1b
brunascauri <brunascauri@hotmail.com>
2025-02-25 16:08:08 -0300

    fix: search page layout

--- lib/screens/search/search_page_content.dart
+++ lib/screens/search/search_page_content.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 
+import 'package:test_olliefy/utils/colors.dart';
 import 'package:test_olliefy/utils/styles/texts.dart';
 import 'package:test_olliefy/components/atoms/drag_bar.dart';
 
@@ -12,44 +13,71 @@ class SearchPageContent extends StatelessWidget {
   Widget build(BuildContext context) {
     return Padding(
       padding: const EdgeInsets.all(16.0),
-      child: CustomScrollView(
-      controller: scrollController,
-        slivers: [
-          SliverToBoxAdapter(
-            child: Padding(
-              padding: const EdgeInsets.only(top: 24, bottom: 8),
-              child: DragBar(),
-            )
-          ),
-          SliverToBoxAdapter(
-            child: Padding(
-            padding: const EdgeInsets.only(bottom: 8),
-              child: GestureDetector(
-                onTap: () => Navigator.pop(context),
-                child: Text(
-                  'Search',
-                  style: TextsStyles.heading2Bold(),
+      child: Scaffold(
+        backgroundColor: AppColors.primaryWhite,
+        body: CustomScrollView(
+          controller: scrollController,
+          slivers: [
+            SliverToBoxAdapter(
+              child: Padding(
+                padding: const EdgeInsets.only(top: 24),
+                child: DragBar(),
+              )
+            ),
+            SliverToBoxAdapter(
+              child: Padding(
+              padding: const EdgeInsets.only(bottom: 24, top: 40),
+                child: Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  children: [
+                    GestureDetector(
+                      onTap: () => Navigator.pop(context),
+                      child: Text(
+                        'Search',
+                        style: TextsStyles.heading2Bold(),
+                      ),
+                    ),
+                    IconButton(
+                      icon: Icon(Icons.close),
+                      iconSize: 24,
+                      color: AppColors.primaryGray30,
+                      onPressed: () {
+                        Navigator.pop(context);
+                      },
+                    ),
+                  ]
                 ),
               )
-            )
-          ),
-          SliverToBoxAdapter(
-            child: Padding(
-            padding: const EdgeInsets.only(bottom: 16),
-              child: Container()
-            )
-          ),
-          SliverToBoxAdapter(
-            child: Padding(
-            padding: const EdgeInsets.only(bottom: 8),
+            ),
+            SliverToBoxAdapter(
+              child: Padding(
+              padding: const EdgeInsets.only(bottom: 48),
+                child: TextFormField(
+                  decoration: InputDecoration(
+                    prefixIcon: Icon(Icons.search, color: Colors.grey),
+                    hintText: 'Search spots, users or items',
+                    border: OutlineInputBorder(
+                      borderRadius: BorderRadius.circular(3.0),
+                      borderSide: BorderSide(color: AppColors.primaryGold70),
+                    ),
+                    focusedBorder: OutlineInputBorder(
+                      borderSide: BorderSide(color: AppColors.primaryGold70, width: 3.0),
+                    ),
+                    contentPadding: EdgeInsets.symmetric(vertical: 12.0, horizontal: 16.0),
+                  ),
+                  onChanged: (value) {
+                    print("Search query: $value");
+                  },
+                ),
+              )
+            ),
               child: Text(
                 'LAST POSTS',
                 style: TextsStyles.spacedGray(),
               ),
-            )
-          ),
-        ],
-      ),
+          ],
+        ),
+      )
     );
   }
 }
