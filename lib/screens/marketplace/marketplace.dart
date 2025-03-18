6756e16df1862458d17bcecb461c8a22a836cd57
brunascauri <brunascauri@hotmail.com>
2025-03-18 11:41:24 -0300

    feat: add search bar and filter button

--- lib/screens/marketplace/marketplace.dart
+++ lib/screens/marketplace/marketplace.dart
@@ -26,6 +26,43 @@ class _MarketplaceState extends State<Marketplace> {
       backgroundColor: AppColors.primaryWhite,
       body: CustomScrollView(
         slivers:[
+          SliverToBoxAdapter(
+            child: Padding(
+              padding: const EdgeInsets.only(left: 16, right: 16, top: 24),
+              child: Row(
+                children: [
+                  Expanded(
+                    child: TextFormField(
+                      controller: _searchController,
+                      decoration: InputDecoration(
+                        prefixIcon: Icon(Icons.search, color: Colors.grey),
+                        hintText: 'Search Articles',
+                        border: OutlineInputBorder(
+                          borderRadius: BorderRadius.circular(3.0),
+                          borderSide: BorderSide(color: AppColors.primaryGold70),
+                        ),
+                        focusedBorder: OutlineInputBorder(
+                          borderSide: BorderSide(color: AppColors.primaryGold70, width: 3.0),
+                        ),
+                        contentPadding: EdgeInsets.symmetric(vertical: 12.0, horizontal: 16.0),
+                      ),
+                      onChanged: (value) {
+                        print("Search query: $value");
+                      },
+                    ),
+                  ),
+                  SizedBox(width: 22),
+                  IconButton(
+                    icon: Image.asset('assets/map_page/icons/filter_icon.png', width: 18),
+                    iconSize: 24,
+                    onPressed: () {
+                      // Navigator.pop(context);
+                    },
+                  ),
+                ],
+              ),
+            ),
+          ),
         ]
       ),
     );
