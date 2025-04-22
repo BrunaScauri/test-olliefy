1d24ed640ea7b4a92778591d82c939dea1fa5866
brunascauri <brunascauri@hotmail.com>
2025-04-22 17:43:01 -0300

    feat: add success snackbar to 'save' icon

--- lib/components/molecules/create_page/post_recording_sidebar_actions.dart
+++ lib/components/molecules/create_page/post_recording_sidebar_actions.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 
 import 'package:test_olliefy/components/atoms/create_page/sidebar_action_icon.dart';
+import 'package:test_olliefy/components/atoms/create_page/success_snackbar.dart';
 import 'package:test_olliefy/components/molecules/modal_bottom_sheet.dart';
 import 'package:test_olliefy/screens/create/post_settings_bottom_sheet/post_visibility_bottom_sheet.dart';
 import 'package:test_olliefy/screens/create/post_settings_bottom_sheet/post_type_bottom_sheet.dart';
@@ -23,7 +24,16 @@ class PostRecordingSidebarActions extends StatelessWidget {
           ModalBottomSheet(onTap: () => PostTypeBottomSheet(), child: SidebarActionIcon(text: 'Type', imagePath: 'assets/create_page/icons/type_icon.png')),
           ModalBottomSheet(onTap: () => PostObstaclesBottomSheet(), child: SidebarActionIcon(text: 'Obstacles', imagePath: 'assets/create_page/icons/obstacles_icon.png')),
           ModalBottomSheet(onTap: () => PostDescriptionBottomSheet(), child: SidebarActionIcon(text: 'Description', imagePath: 'assets/create_page/icons/description_icon.png')),
-          ModalBottomSheet(onTap: () => Container(), child: SidebarActionIcon(text: 'Save', imagePath: 'assets/create_page/icons/save_icon.png')),
+          GestureDetector(
+            onTap: () => showSuccessSnackbar(
+              context,
+              label: 'Video saved successfully',
+            ),
+            child: SidebarActionIcon(
+              text: 'Save',
+              imagePath: 'assets/create_page/icons/save_icon.png',
+            ),
+          ),
         ]
       )
     );
