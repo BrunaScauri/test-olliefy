84e47e1cb8c626cd8fd571d3853243a1dd16966a
brunascauri <brunascauri@hotmail.com>
2025-04-24 16:46:42 -0300

    feat: add bottom modal sheet to create/edit/spot page

--- lib/screens/create/post_settings_bottom_sheet/choose_a_spot.dart
+++ lib/screens/create/post_settings_bottom_sheet/choose_a_spot.dart
@@ -4,6 +4,8 @@ import 'package:test_olliefy/utils/colors.dart';
 import 'package:test_olliefy/utils/styles/texts.dart';
 
 import 'package:test_olliefy/screens/app_tab.dart';
+import 'package:test_olliefy/components/atoms/main_confirm_button.dart';
+import 'package:test_olliefy/components/molecules/bottom_sheet_header.dart';
 
 class ChooseASpotScreen extends StatefulWidget {
   const ChooseASpotScreen({super.key});
@@ -13,6 +15,131 @@ class ChooseASpotScreen extends StatefulWidget {
 }
 
 class _ChooseASpotScreenState extends State<ChooseASpotScreen> {
+  bool _isModalSheetOpen = false;
+  late FocusNode _searchFocusNode;
+  late TextEditingController _searchController;
+
+  void initState() {
+    super.initState();
+    _searchFocusNode = FocusNode();
+    _searchController = TextEditingController();
+    WidgetsBinding.instance.addPostFrameCallback((_) {
+      setState(() {
+        _isModalSheetOpen = true;
+        _openSheet();
+      });
+    });
+  }
+
+  @override
+  void dispose() {
+    _searchFocusNode.dispose();
+    super.dispose();
+  }
+
+  void _openSheet() {
+    String? _selectedOption = '';
+    showModalBottomSheet(
+      isScrollControlled: true,
+      context: context,
+      shape: RoundedRectangleBorder(
+        borderRadius: BorderRadius.only(topRight: Radius.circular(20), topLeft: Radius.circular(20)),
+      ),
+      builder: (context) {
+        return StatefulBuilder(
+          builder: (context, setModalState) {
+            return Wrap(
+              children: [
+                Container(
+                  child: Padding(
+                    padding: EdgeInsets.only(left: 16, right: 16, bottom: 8),
+                    child: Column(
+                      children: [
+                        BottomSheetHeader(title: 'SPOT'),
+                        SizedBox(height: 16),
+                        TextFormField(
+                          focusNode: _searchFocusNode,
+                          controller: _searchController,
+                          onTap: () {
+                          },
+                          decoration: InputDecoration(
+                            prefixIcon: Icon(Icons.search, color: Colors.grey),
+                            suffixIcon: (_searchController.text.isNotEmpty)
+                              ? IconButton(
+                                  icon: Image.asset('assets/map_page/icons/go_search_icon.png'),
+                                  onPressed: () {
+                                  },
+                                )
+                              : null,
+                            hintText: 'Fix: Placeholder text',
+                            border: OutlineInputBorder(
+                              borderRadius: BorderRadius.circular(3.0),
+                              borderSide: BorderSide(color: AppColors.primaryGold70),
+                            ),
+                            focusedBorder: OutlineInputBorder(
+                              borderSide: BorderSide(color: AppColors.primaryGold70, width: 3.0),
+                            ),
+                            contentPadding: EdgeInsets.symmetric(vertical: 12.0, horizontal: 16.0),
+                          ),
+                          onChanged: (value) {
+                          },
+                        ),
+                        SizedBox(height: 32),
+                        Row(
+                          children: [
+                            Text('CONDITION', style: TextsStyles.spacedGray()),
+                          ],
+                        ),
+                        SizedBox(height: 24),
+                        Row(
+                          children: [
+                            Padding(
+                              padding: EdgeInsets.only(right: 30),
+                              child: Row(
+                                children: [
+                                  Radio<String>(
+                                    value: 'regular',
+                                    groupValue: _selectedOption,
+                                  ),
+                                  SizedBox(width: 20),
+                                  Text('Regular', style: ChallengesStyles.joinTheChallengeText()),
+                                ],
+                              ),
+                            ),
+                            Padding(
+                              padding: EdgeInsets.only(right: 30),
+                              child: Row(
+                                children: [
+                                  Radio<String>(
+                                    value: 'good',
+                                    groupValue: _selectedOption,
+                                    onChanged: (v) => setState(() => _selectedOption = v),
+                                  ),
+                                  SizedBox(width: 20),
+                                  Text('Good', style: ChallengesStyles.joinTheChallengeText()),
+                                ],
+                              )
+                            ),
+                          ],
+                        ),
+                        SizedBox(height: 32),
+                        MainConfirmButton(title: 'Confirm', onTap: () => {Navigator.pop(context)}, minimumSize: Size(MediaQuery.of(context).size.width, 60)),
+                      ],
+                    ),
+                  ),
+                ),
+              ],
+            );
+          }
+        );
+      },
+      ).whenComplete(() {
+        setState(() {
+          _isModalSheetOpen = false;
+        });
+      });
+    }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -63,23 +190,8 @@ class _ChooseASpotScreenState extends State<ChooseASpotScreen> {
               ),
             ],
           ),
-        ],
-      )
+        ]
+      ),
     );
   }
 }
-
-
-// showModalBottomSheet<dynamic>(
-//           isScrollControlled: true,
-//           context: context,
-//           shape: RoundedRectangleBorder(
-//             borderRadius: BorderRadius.only(
-//               topRight: Radius.circular(20),
-//               topLeft: Radius.circular(30),
-//             ),
-//           ),
-//           builder: (BuildContext context) {
-//             return const ChooseASpotScreen();
-//           },
-//         );
