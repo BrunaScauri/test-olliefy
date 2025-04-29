d1f3d270bc9b82515c1ee12567baddf8c43c5e09
brunascauri <brunascauri@hotmail.com>
2025-04-29 10:55:33 -0300

    chore: remove unused imports

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,5 @@
-import 'dart:io';
-
-import 'package:firebase_core/firebase_core.dart';
-import 'package:test_olliefy/firebase_options.dart';
 import 'package:flutter/material.dart';
+import 'package:firebase_core/firebase_core.dart';
 import 'package:provider/provider.dart';
 import 'package:media_kit/media_kit.dart';
 
