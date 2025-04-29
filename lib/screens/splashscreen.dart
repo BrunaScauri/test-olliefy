055d77c281a310b91efc2032abfdd5b351575297
brunascauri <brunascauri@hotmail.com>
2025-04-29 10:34:34 -0300

    style: indentation

--- lib/screens/splashscreen.dart
+++ lib/screens/splashscreen.dart
@@ -51,11 +51,11 @@ class Splashscreen extends StatefulWidget {
         body: SafeArea( 
           child: 
           Column(children: [  
-          Flexible(flex: 5, child: Container()),
-          SvgPicture.asset('assets/skateboard_only_logo.svg', colorFilter: const ColorFilter.mode(AppColors.primaryGold30, BlendMode.srcIn), height: 100),
-          const SizedBox(height: 48),
-          Flexible(flex: 4, child: Container()),
-          const Image(image: AssetImage('assets/splashscreen/Animation.gif'), height: 100, width: 200),
+            Flexible(flex: 5, child: Container()),
+            SvgPicture.asset('assets/skateboard_only_logo.svg', colorFilter: const ColorFilter.mode(AppColors.primaryGold30, BlendMode.srcIn), height: 100),
+            const SizedBox(height: 48),
+            Flexible(flex: 4, child: Container()),
+            const Image(image: AssetImage('assets/splashscreen/Animation.gif'), height: 100, width: 200),
           Container(
             width: 80,
             child: LinearProgressIndicator(value: _progressValue, valueColor: AlwaysStoppedAnimation<Color>(Color.fromRGBO(222, 199, 55, 1)), backgroundColor: Colors.white),
