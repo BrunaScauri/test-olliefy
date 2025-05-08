538717b80e5c0a80bb38be6d20fe39cfc3d9f12c
brunascauri <brunascauri@hotmail.com>
2025-05-08 18:30:27 -0300

    test: fix to reflect actual app

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -8,23 +8,18 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
 
-import 'package:test_olliefy/main.dart';
+import 'package:test_olliefy/screens/main_screen.dart';
 
 void main() {
   testWidgets('Counter increments smoke test', (WidgetTester tester) async {
     // Build our app and trigger a frame.
-    await tester.pumpWidget(const MyApp());
+    await tester.pumpWidget(MaterialApp(home: MainScreen()));
+    await tester.pumpAndSettle();
 
-    // Verify that our counter starts at 0.
-    expect(find.text('0'), findsOneWidget);
-    expect(find.text('1'), findsNothing);
+    final loginBtn = find.byKey(const Key('login_button'));
+    expect(loginBtn, findsOneWidget);
 
-    // Tap the '+' icon and trigger a frame.
-    await tester.tap(find.byIcon(Icons.add));
-    await tester.pump();
-
-    // Verify that our counter has incremented.
-    expect(find.text('0'), findsNothing);
-    expect(find.text('1'), findsOneWidget);
+    final registerBtn = find.byKey(const Key('register_button'));
+    expect(registerBtn, findsOneWidget);
   });
 }
