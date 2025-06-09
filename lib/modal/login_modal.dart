9bdfd466ef1cd91acc83832857a74c21b5640dee
brunascauri <brunascauri@hotmail.com>
2025-06-09 14:00:59 -0300

    fix: switch container for safeArea

--- lib/modal/login_modal.dart
+++ lib/modal/login_modal.dart
@@ -128,108 +128,109 @@ class _LoginModalState extends State<LoginModal> with WidgetsBindingObserver {
 
   @override
   Widget build(BuildContext context) {
-    return Container(
-      decoration: BoxDecoration(
-        borderRadius: BorderRadius.vertical(top: Radius.circular(16)),
-        color: AppColors.primaryWhite,
-      ),
-      padding: const EdgeInsets.all(16.0),
-      child: Column(
-        mainAxisSize: MainAxisSize.min,
-        children: [
-          BottomSheetHeader(
-            title: 'LOG IN',
-          ),
-          //textfield for login and button
-          Padding(
-            padding: EdgeInsets.symmetric(vertical: 35.0),
-            child: Column(
-              children: [
-                TextFormField(
-                  controller: _controller,
-                  onChanged: (value) {
-                    setState(() {
-                      inputText = value;
-                    });
-                  },
-                  decoration: FormDecorations.textFieldDecoration(labelText: 'Phone, email or username'),
-                ),
-                SizedBox(height: 16),
-                ElevatedButton(
-                  onPressed:
-                  _isButtonEnabled ? () {
-                    checkIfUserExists(inputText);
-                  } : null,
-                  style: _isButtonEnabled ? ButtonStyles.elevatedButton(backgroundColor: AppColors.primaryBlack) : ButtonStyles.elevatedButton(backgroundColor: AppColors.buttonDisabled12),
-                  child: Row(
-                    mainAxisAlignment: MainAxisAlignment.center,
-                    children: [
-                      RichText(
-                        text: TextSpan(
-                          style: _isButtonEnabled 
-                          ? TextStyles.elevatedButtonText(color: AppColors.primaryWhite) 
-                          : TextStyles.elevatedButtonText(color: AppColors.primaryGray10),
-                          children: [
-                            TextSpan(text: 'Log in'),
-                          ],
-                        ),
-                      ),
-                    ]
-                  ),
-                ),
-              ]
-            )
-          ),
-          Divider(height: 10, thickness: 0.8),
-          //socials buttons
-          Padding(
-            padding: EdgeInsets.symmetric(vertical: 40.0),
-            child: Column(
-              children: [
-                CustomButtons.socialsButton(
-                  backgroundColor: AppColors.primaryWhite,
-                  assetPath: 'assets/google_logo.svg',
-                  buttonText: 'Continue with Google',
-                  textColor: AppColors.primaryBlack,
-                  onPressed: () {
-                    signinUserByGoogle();
-                  },
-                ),
-                const SizedBox(height: 16),
-                CustomButtons.socialsButton(
-                  backgroundColor: AppColors.primaryBlack,
-                  assetPath: 'assets/apple_logo.svg',
-                  buttonText: 'Continue with Apple',
-                  textColor: AppColors.primaryWhite,
-                  onPressed: () {
-                    //todo: wait for apple developer program account
-                  },
-                ),
-
-              ]
-            )
-          ),
-          RichText(
-            text: TextSpan(
-              style: GoogleFonts.openSans(fontSize: 14, color: Colors.black),
-              children: [
-                TextSpan(text: 'By logging in, you are accepting our ',
-                    style: TextStyle(color: AppColors.primaryBlack)),
-                TextSpan(
-                  text: 'Terms and Conditions ',
-                  style: TextsStyles.termsAndConditionsBold(),
-                ),
-                TextSpan(text: 'as well as the '),
-                TextSpan(
-                  text: 'Privacy Policy.',
-                  style: TextsStyles.termsAndConditionsBold(),
-                ),
-              ],
+    return SafeArea(
+      child: Container(
+        decoration: BoxDecoration(
+          borderRadius: BorderRadius.vertical(top: Radius.circular(16)),
+          color: AppColors.primaryWhite,
+        ),
+        padding: const EdgeInsets.all(16.0),
+        child: Column(
+          mainAxisSize: MainAxisSize.min,
+          children: [
+            BottomSheetHeader(
+              title: 'LOG IN',
             ),
-          ),
-          Flexible(child: Container())
-        ],
-      ),
+            //textfield for login and button
+            Padding(
+              padding: EdgeInsets.symmetric(vertical: 35.0),
+              child: Column(
+                children: [
+                  TextFormField(
+                    controller: _controller,
+                    onChanged: (value) {
+                      setState(() {
+                        inputText = value;
+                      });
+                    },
+                    decoration: FormDecorations.textFieldDecoration(labelText: 'Phone, email or username'),
+                  ),
+                  SizedBox(height: 16),
+                  ElevatedButton(
+                    onPressed:
+                    _isButtonEnabled ? () {
+                      checkIfUserExists(inputText);
+                    } : null,
+                    style: _isButtonEnabled ? ButtonStyles.elevatedButton(backgroundColor: AppColors.primaryBlack) : ButtonStyles.elevatedButton(backgroundColor: AppColors.buttonDisabled12),
+                    child: Row(
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      children: [
+                        RichText(
+                          text: TextSpan(
+                            style: _isButtonEnabled 
+                            ? TextStyles.elevatedButtonText(color: AppColors.primaryWhite) 
+                            : TextStyles.elevatedButtonText(color: AppColors.primaryGray10),
+                            children: [
+                              TextSpan(text: 'Log in'),
+                            ],
+                          ),
+                        ),
+                      ]
+                    ),
+                  ),
+                ]
+              )
+            ),
+            Divider(height: 10, thickness: 0.8),
+            //socials buttons
+            Padding(
+              padding: EdgeInsets.symmetric(vertical: 40.0),
+              child: Column(
+                children: [
+                  CustomButtons.socialsButton(
+                    backgroundColor: AppColors.primaryWhite,
+                    assetPath: 'assets/google_logo.svg',
+                    buttonText: 'Continue with Google',
+                    textColor: AppColors.primaryBlack,
+                    onPressed: () {
+                      signinUserByGoogle();
+                    },
+                  ),
+                  const SizedBox(height: 16),
+                  CustomButtons.socialsButton(
+                    backgroundColor: AppColors.primaryBlack,
+                    assetPath: 'assets/apple_logo.svg',
+                    buttonText: 'Continue with Apple',
+                    textColor: AppColors.primaryWhite,
+                    onPressed: () {
+                      //todo: wait for apple developer program account
+                    },
+                  ),
+                ]
+              )
+            ),
+            RichText(
+              text: TextSpan(
+                style: GoogleFonts.openSans(fontSize: 14, color: Colors.black),
+                children: [
+                  TextSpan(text: 'By logging in, you are accepting our ',
+                      style: TextStyle(color: AppColors.primaryBlack)),
+                  TextSpan(
+                    text: 'Terms and Conditions ',
+                    style: TextsStyles.termsAndConditionsBold(),
+                  ),
+                  TextSpan(text: 'as well as the '),
+                  TextSpan(
+                    text: 'Privacy Policy.',
+                    style: TextsStyles.termsAndConditionsBold(),
+                  ),
+                ],
+              ),
+            ),
+            Flexible(child: Container())
+          ],
+        ),
+      )
     );
   }
 }
