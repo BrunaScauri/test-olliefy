aede002729fae29dd1cf5cadd8993d1b54e5ff59
brunascauri <brunascauri@hotmail.com>
2025-06-25 17:41:00 -0300

    fix: route for logout button

--- lib/screens/profile/user_profile.dart
+++ lib/screens/profile/user_profile.dart
@@ -1,5 +1,6 @@
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
+import 'package:test_olliefy/screens/main_screen.dart';
 import 'package:url_launcher/url_launcher.dart';
 
 import 'package:google_fonts/google_fonts.dart';
